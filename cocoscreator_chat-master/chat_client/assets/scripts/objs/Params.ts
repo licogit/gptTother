9c251a23dd86804be2c2ef1caa0c03cee49d03f4
lico <549063037@qq.com>
2025-09-05 18:17:27 +0800

    add

---
+++ cocoscreator_chat-master/chat_client/assets/scripts/objs/Params.ts
@@ -0,0 +1,15 @@
+export interface User{
+    id:string; //用户的id
+    name:string; //用户名
+    password:string; //密码
+    channel:string; //当前的频道
+    imgUrl:string; //用户头像的图片地址 
+}
+
+export interface Message{
+    sendName:string;     //发送的用户昵称
+    sendId:string;    //发送给的人 没有则发送全部
+    msg:string;    //发送的信息
+    channel:string; //当前的频道
+    
+}
